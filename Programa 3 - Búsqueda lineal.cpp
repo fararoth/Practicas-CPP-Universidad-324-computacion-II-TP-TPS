729a784db4cf4c10f8c0e6d316f6bb079282a9a3
Luis Farias <60280254+fararoth@users.noreply.github.com>
2023-06-18 19:14:06 -0400

    Update Programa 3 - Búsqueda lineal.cpp
    Código colocado (después que las evaluaciones fueron cerradas para que nadie se copiara)

--- Programa 3 - Búsqueda lineal.cpp
+++ Programa 3 - Búsqueda lineal.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, busqueda, i;
+    bool encontrado = false;
+    cout << "Ingrese la cantidad de elementos: ";
+    cin >> n;
+    int arr[n];
+    for (i = 0; i < n; i++) {
+        cout << "Ingrese el elemento " << i+1 << ": ";
+        cin >> arr[i];
+    }
+    cout << "Ingrese el elemento a buscar: ";
+    cin >> busqueda;
+    for (i = 0; i < n; i++) {
+        if (arr[i] == busqueda) {
+            cout << "El elemento " << busqueda << " se encuentra en la posición " << i+1 << " del arreglo." << endl;
+            encontrado = true;
+            break;
+        }
+    }
+    if (!encontrado) {
+        cout << "El elemento " << busqueda << " no se encuentra en el arreglo." << endl;
+    }
+    system("pause");
+    return 0;
+}
 
